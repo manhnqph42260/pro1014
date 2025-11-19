0b1c1693228b3051b705b857d8558f8fc8150b03
manhnq <manhnqph42260@fpt.edu.vn>
2025-11-19 19:24:29 +0700

    Remove redundant comment about using match for controller handling

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ require_once './models/ProductModel.php';
 // Route
 $act = $_GET['act'] ?? '/';
 
-
-// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
-
 match ($act) {
     // Trang chủ
     '/'=>(new ProductController())->Home(),
